49adfb4bb5ee1427c9083a0993bebdcb628f3853
Jpsix <idjasonlin@gmail.com>
2022-07-21 17:44:43 +0800

    change to c++ std=14

--- gen_sysc.sh
+++ gen_sysc.sh
@@ -4,8 +4,8 @@ tar -zxvf systemc-2.3.3.tar.gz
 rm systemc-2.3.3.tar.gz
 mkdir systemc-2.3.3-built
 cd systemc-2.3.3
-cmake -B build -DCMAKE_INSTALL_PREFIX=../systemc-2.3.3-built
-cd build
+cmake -B cmake_build -DCMAKE_INSTALL_PREFIX=../systemc-2.3.3-built -DCMAKE_CXX_STANDARD=14
+cd cmake_build
 make
 make install
 
